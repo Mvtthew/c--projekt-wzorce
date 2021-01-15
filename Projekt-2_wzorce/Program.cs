e81034f684be063f2f1714c5094ab800c60d1b77
Mateusz Ożóg <mateusz2345678@gmail.com>
2021-01-15 06:32:05 +0100

    init

---
+++ Projekt-2_wzorce/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Projekt_2_wzorce
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
